7369b6fc229a34159395022796625f123d41bbe1
bhanupalagati <bhanupalagati@gmail.com>
2022-01-15 21:56:58 -0500

    creating the card component

---
+++ frontend/src/app/interfaces/product.interface.ts
@@ -0,0 +1,24 @@
+export type Product = {
+    title: string,
+    secondary_title: string,
+    imageUrl: string,
+    price: number,
+    simple_desc: string,
+    description: string,
+    city: string,
+    state: string,
+    location_lat: number,
+    location_long: number,
+    posted_by: string,
+    posted_date: string,
+    condition: "New" | "Like New" | "Used" | "Heavily Used" | "NA",
+    age: number,
+    status: "Deleted" | "Sold" | "Active"
+}
+
+export type ProductResponse = {
+    count: number,
+    next_page: number,
+    previous_page: number,
+    products: Product[]
+}
