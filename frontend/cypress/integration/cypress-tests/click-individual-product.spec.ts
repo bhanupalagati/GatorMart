4b0969af11847491def87ca9f5f865c4c21b279d
nitinrameshuf <nitinramesh@ufl.edu>
2022-03-04 13:06:28 -0500

    product verificaiton

--- frontend/cypress/integration/cypress-tests/click-individual-product.spec.ts
+++ frontend/cypress/integration/cypress-tests/click-individual-product.spec.ts
@@ -5,12 +5,41 @@ describe('Access Test', function () {
     beforeEach(() => {
         cy.visit('http://localhost:4200/')
     })
+
     it('Click 1st product and move into detailed view', function() {
-        cy.get('mat-card').click()
+        cy.get('mat-card').eq(1).click()
         cy.wait(1500)
         cy.scrollTo('bottom')
         cy.wait(1500)
         cy.scrollTo('top') 
+        
+  //  })
+    //Also check the carousel
+  //  it('Control the carousel movement', function() {
+        cy.get('app-product-details')
+        .find('ngb-carousel')
+        .find('a')
+        .eq(1)
+        .click()
+        cy.wait(1000)
+        cy.get('app-product-details')
+        .find('ngb-carousel').find('a').eq(1)
+        .click()
+        cy.wait(1000)
+        cy.get('app-product-details')
+        .find('ngb-carousel').find('a')
+        .eq(1)
+        .click()
+        cy.wait(1000)
+        cy.get('app-product-details')
+        .find('ngb-carousel')
+        .find('a').eq(0)
+        .click()
+        cy.wait(1000)
+        cy.get('app-product-details')
+        .find('ngb-carousel').find('a').eq(0)
+        .click()
+        cy.wait(1000)
         
     })
 })
