d98ab35c4090f692a861bbbedcd319b96bac66a9
nitinrameshuf <nitinramesh@ufl.edu>
2022-04-01 16:32:10 -0400

    Merge branch 'main' of https://github.com/bhanupalagati/GatorMart into 98-user-location-integration

--- frontend/cypress/integration/cypress-tests/registration-check-test.spec.ts
+++ frontend/cypress/integration/cypress-tests/registration-check-test.spec.ts
@@ -1,1 +1,39 @@
 //Objective: Test the registration functionality
+
+/// <reference types="cypress" />
+
+describe('Register an user normally', function () {
+    it('Sign up functionality', function() {
+        cy.visit('http://localhost:4200/signup')
+        
+    })
+
+    it('Sign up form', function() {
+        //Email
+        cy.get('input')
+        .eq(0).type('user@gatormart.com') 
+        //Password
+        cy.get('input')
+        .eq(1).type('Password1$') 
+        //Re-Type Password
+        cy.get('input')
+        .eq(2).type('Password1$') 
+        //Profession
+        cy.get('input')
+        .eq(3).type('Student')
+        //Name
+        cy.get('input')
+        .eq(4).type('Gatory')
+        //Last name
+        cy.get('input')
+        .eq(5).type('Marty')
+        //Last name
+        cy.get('input')
+        .eq(6).invoke('removeAttr','type').type('05-02-2011');
+    })
+    it('Submit registration form', function() {
+        cy.get('button').click()       
+    })
+})
+
+
