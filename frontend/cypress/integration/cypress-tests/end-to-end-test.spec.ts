a3e35fc115678f13dbb335c70a0feb7b45c3eb77
nitinrameshuf <nitinramesh@ufl.edu>
2022-04-13 23:56:45 -0400

    e2e cypress2

--- frontend/cypress/integration/cypress-tests/end-to-end-test.spec.ts
+++ frontend/cypress/integration/cypress-tests/end-to-end-test.spec.ts
@@ -1,9 +1,41 @@
 // Objective: Complete end to end test Cypress video
 /// <reference types ="cypress" />
 
-describe('Login', function () {
-    it('Sign up functionality', function() {
+describe('Register an user', function () {
+    it('Sign up', function() {
         cy.visit('http://localhost:4200/signup')
+        
+    })
+
+    it('Sign up form', function() {
+        //Email
+        cy.get('input')
+        .eq(0).type('newuser@gatormart.com') 
+        //Password
+        cy.get('input')
+        .eq(1).type('Password2$') 
+        //Re-Type Password
+        cy.get('input')
+        .eq(2).type('Password2$') 
+        //Profession
+        cy.get('input')
+        .eq(3).type('Faculty')
+        //Name
+        cy.get('input')
+        .eq(4).type('Gatory')
+        //Last name
+        cy.get('input')
+        .eq(5).type('Marty')
+        //Last name
+        cy.get('input')
+        .eq(6).invoke('removeAttr','type').type('05-02-2011');
+    })
+    it('Submit registration form', function() {
+        cy.get('button').click()       
+    })
+    it('Login functionality', function() {
+        //check this
+        cy.visit('http://localhost:4200/')
         })
     it('Login function', function() {
         //Email
