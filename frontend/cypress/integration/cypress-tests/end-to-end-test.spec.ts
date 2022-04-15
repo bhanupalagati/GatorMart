e0d45f0583bdf4b8f7c469ad0802427e7054d216
nitinrameshuf <nitinramesh@ufl.edu>
2022-04-15 15:28:41 -0400

    e2e2

--- frontend/cypress/integration/cypress-tests/end-to-end-test.spec.ts
+++ frontend/cypress/integration/cypress-tests/end-to-end-test.spec.ts
@@ -46,4 +46,81 @@ describe('Register an user', function () {
     it('Submit Button', function() {
         cy.get('button').click()       
     })
+    it('Access the page', function() {
+        cy.visit('http://localhost:4200/create')
+        
+    })
+
+    it('Interacting with the 1st image field', function() {
+        //adding the relative path - get the images for the fixtures
+        const main_image = 'cypress/fixtures/images/elec.jpg'
+        cy.get('div [class="ad_form_container__flex__input"]')
+        .eq(0)
+        .find('input').selectFile(main_image)        
+    })
+    it('Interacting with the 2nd image field', function() {
+        const detailed_images = 'cypress/fixtures/images/gadgets.jpeg'
+        cy.get('div [class="ad_form_container__flex__input"]')
+        .eq(1)
+        .find('input').selectFile(detailed_images)        
+    })
+    //Needs verification
+    it('Form text fields', function() {
+        //Title
+        cy.get('input')
+        .eq(2)
+        .type('Cypress Electronics') 
+        //Seconday Title
+        cy.get('input')
+        .eq(3)
+        .type('Secondary title Electronics') 
+        cy.get('input')
+        .eq(5)
+        .clear()
+        .type('1500')
+        //Simple Description
+        cy.get('input')
+        .eq(6)
+        .type('Simple Description for Cypress Iphone 1')
+        //Description
+        cy.get('input')
+        .eq(7)
+        .type('Description for Cypress Iphone 1')
+        //City
+        cy.get('input')
+        .eq(8)
+        .type('Mysoreville')
+        //State
+        cy.get('input')
+        .eq(9)
+        .type('Florida')
+        //Latitude
+        cy.get('input')
+        .eq(10)
+        .type('12.1')
+        //Longitude
+        cy.get('input')
+        .eq(11)
+        .type('15.1')
+        //Condition
+        cy.get('input')
+        .eq(12)
+        .clear()
+        .type('New')
+        //Age
+        cy.get('input')
+        .eq(13)
+        .clear()
+        .type('5')
+        cy.get('input')
+        .eq(15)
+        .type('Student')
+        //Category
+        cy.get('input')
+        .eq(16)
+        .type('Electronics')
+    })
+    it('Submit the form', function() {
+        cy.get('button').click()       
+    })
 })
