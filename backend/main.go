1499e2a063de5aede62f0b11bc824820351c5985
vamsikrishnachinnu <vamsi.kommareddy@gmail.com>
2022-04-20 18:42:10 -0400

    deployement heroku

--- backend/main.go
+++ backend/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"main/products"
+	"os"
 
 	//swagger "github.com/arsmn/fiber-swagger"
 	swagger "github.com/arsmn/fiber-swagger/v2"
@@ -88,7 +89,7 @@ func main() {
 	// app.Get("/login", Login)
 	// app.Post("/user", users.SaveUser)
 	// default
-	log.Fatal(app.Listen(":8000"))
-	// log.Fatal(app.Listen(":" + os.Getenv("PORT")))
+	//log.Fatal(app.Listen(":8000"))
+	log.Fatal(app.Listen(":" + os.Getenv("PORT")))
 
 }
